5689779e97662024eb2ce0eb614a283b2671835d
wangdejun <1074201799@qq.com>
2017-12-25 10:24:08 +0800

    adjust the imgs

--- christ.py
+++ christ.py
@@ -1,39 +1,49 @@
 import dlib
 import cv2
 
+#get the image needed
 hat_img = cv2.imread("img/hat_img.png")
-img = cv2.imread("img/Torres.jpg")
+img = cv2.imread("img/1.jpg")
 
+#split the channel of the hat img
 r,g,b = cv2.split(hat_img)
 rgb_hat = cv2.merge((r,g,b))
+# can't split the alpha channel, replace it with red channel.
 cv2.imwrite("img/hat_alpha.jpg", r)
 
+# human face keypoint detector
 predictor_path = "model/shape_predictor_5_face_landmarks.dat"
 predictor = dlib.shape_predictor(predictor_path)
+# huamn face detector
 detector = dlib.get_frontal_face_detector()
+# face detect
 dets = detector(img, 1)
 
-
+#if detected a human face in the picture
 if len(dets)>0:
     for d in dets:
-        x,y,w,h = d.left(), d.top(), d.right()-d.left(), d.bottom()-d.top() 
+        x,y,w,h = d.left(), d.top(), d.right()-d.left(), d.bottom()-d.top()
         # x,y,w,h = faceRect 
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2,8,0)
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0,0.3),1,8,0)
         shape = predictor(img, d)
+        # key point detect
         for point in shape.parts(): 
             print "length of face points"
             print len(shape.parts())
-            cv2.circle(img, (point.x, point.y), 3, color=(255, 0, 0)) 
+            cv2.circle(img, (point.x, point.y), 1, color=(255, 0, 0))
+        # cv2.imshow("image",img)
+        # cv2.waitKey()
 
-# pick the lefteye point and the righteye point
+
+# pick the left eye point and the right eye point
 point1 = shape.part(0)
-
-print point1
 point2 = shape.part(2)
 
 # calculate the middle point
 eyes_center = ((point1.x+point2.x)/2, (point1.y+point2.y)/2)
-cv2.circle(img,eyes_center,3,color=(0,255,0))
+# cv2.circle(img,eyes_center,3,color=(0,255,0))
+# cv2.imshow("image",img)
+# cv2.waitKey()
 
 # adjust the hat size to the face size
 factor = 1.5
@@ -53,7 +63,7 @@ mask_inv = cv2.bitwise_not(mask)
 dh = 0
 dw = 0
 
-bg_roi = img[y+dh-resized_hat_h:y+dh, x+dw:x+dw+resized_hat_w] 
+# bg_roi = img[y+dh-resized_hat_h:y+dh, x+dw:x+dw+resized_hat_w] 
 bg_roi = img[y+dh-resized_hat_h:y+dh, (eyes_center[0]-resized_hat_w//3):(eyes_center[0] + resized_hat_w//3*2)] 
 
 # extract the zone from Origin image ROI 
@@ -62,8 +72,8 @@ mask_inv = cv2.merge((mask_inv, mask_inv,mask_inv))
 alpha = mask_inv.astype(float)/255 
 
 alpha = cv2.resize(alpha, (bg_roi.shape[1], bg_roi.shape[0])) 
-print("alpha size: ", alpha.shape)
-print("bg_roi size: ", bg_roi.shape)
+# print("alpha size: ", alpha.shape)
+# print("bg_roi size: ", bg_roi.shape)
 bg = cv2.multiply(alpha, bg_roi)
 bg = bg.astype('uint8')
 
@@ -72,9 +82,10 @@ hat = cv2.resize(hat,(bg_roi.shape[1],bg_roi.shape[0]))
 
 # 2 ROI zone plus
 add_hat = cv2.add(bg,hat) 
-cv2.imshow("add_hat",add_hat) 
+cv2.imshow("add_hat", add_hat)
 
-# 
+# put the zone to the origin photo
 img[y+dh-resized_hat_h:y+dh,(eyes_center[0]-resized_hat_w//3):(eyes_center[0]+resized_hat_w//3*2)] = add_hat
+
 cv2.imshow("image", img)
-cv2.waitKey()
+cv2.waitKey()  
